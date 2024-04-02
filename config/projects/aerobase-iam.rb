d4d337d4958e253a1e207feb316c4bb498e1a55a
Yaniv Marom Nachumi <yanivmar@amdocs.com>
2024-04-02 16:26:22 +0300

    Test health_check

--- config/projects/aerobase-iam.rb
+++ config/projects/aerobase-iam.rb
@@ -21,10 +21,12 @@ homepage "https://github.com/aerobase/omnibus-aerobase-iam-server"
 
 # Defaults to C:/aerobase on Windows
 # and /opt/aerobase on all other platforms
-install_dir "#{default_root}/aerobase"
+name "aerobase"
+install_dir  "#{default_root}/#{name}"
 
 build_version IO.read(File.expand_path("../../../VERSION", __FILE__)).strip
 build_iteration 1
+health_check false
 
 # In order to prevent unecessary cache expiration,
 # package and package version overrides are kept in <project-root>/omnibus_overrides.rb
